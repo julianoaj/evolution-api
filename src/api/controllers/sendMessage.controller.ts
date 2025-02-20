0dbc92d47403f0e0a70dbd90d30d05ca0dbda0f4
julianoaj <alison.ajsantos@gmail.com>
2025-02-20 13:43:23 -0300

    🐛 IsEmoji string to sendReaction

--- src/api/controllers/sendMessage.controller.ts
+++ src/api/controllers/sendMessage.controller.ts
@@ -19,8 +19,10 @@ import { BadRequestException } from '@exceptions';
 import { isBase64, isURL } from 'class-validator';
 
 function isEmoji(str: string) {
-  const emojiRegex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])*$/;
-  return str === '' || emojiRegex.test(str);
+  if (str === '') return true;
+  
+  const emojiRegex = /^[\u{1F300}-\u{1F9FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F000}-\u{1F02F}\u{1F0A0}-\u{1F0FF}\u{1F100}-\u{1F64F}\u{1F680}-\u{1F6FF}]$/u;
+  return emojiRegex.test(str);
 }
 
 export class SendMessageController {
@@ -87,7 +89,7 @@ export class SendMessageController {
 
   public async sendReaction({ instanceName }: InstanceDto, data: SendReactionDto) {
     if (!isEmoji(data.reaction)) {
-      throw new BadRequestException('Reaction must be an emoji or empty to remove');
+      throw new BadRequestException('Reaction must be a single emoji or empty string');
     }
     return await this.waMonitor.waInstances[instanceName].reactionMessage(data);
   }
